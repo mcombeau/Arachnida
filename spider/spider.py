9008c9b96d0d499eb7f99c5cabd1ad99ee0ba50a
mcombeau <mcombeau@student.42.fr>
2023-10-07 17:50:22 +0200

    improve parsing + add defaults

--- spider/spider.py
+++ spider/spider.py
@@ -1,35 +1,40 @@
-import sys
 import argparse
 import pathlib
+
+DEFAULT_DEPTH = 5
+DEFAULT_PATH = './data'
+
+Args = argparse.Namespace
+Parser = argparse.ArgumentParser
 
 # ---------------------------
 # Argument parsing
 # ---------------------------
-def parse_args():
-    parser = argparse.ArgumentParser(description = 'An image scrapper')
+def parse_args() -> Args:
+    parser: Parser = Parser(description = 'An image scrapper')
     parser.add_argument('-r', '--recursive', action = 'store_true', help = 'download images recursively (to depth level 5 by default)')
     parser.add_argument('-l', '--level', dest = 'depth', type = int, help = 'depth level of recursive image search')
     parser.add_argument('-p', '--path', type = pathlib.Path, help = 'path to save downloaded images')
-    parser.add_argument('URL', help = 'URL to crawl')
-    args = parser.parse_args()
+    parser.add_argument('URL', help = 'URL to download images from')
+    args: Args = parser.parse_args()
     if args.recursive and (args.depth is None):
-        args.depth = 5
+        args.depth = DEFAULT_DEPTH
     if args.depth and (args.recursive is False):
         parser.error("argument -l/--level: expected -r/--recursive argument.")
-    print(args)
+    if args.path is None:
+        args.path = pathlib.Path(DEFAULT_PATH)
     return args
-
 
 # ---------------------------
 # Execution
 # ---------------------------
-def crawl(options: dict):
+def crawl() -> None:
     print("Ready to crawl")
 
 # ---------------------------
 # Prettify
 # ---------------------------
-def print_header():
+def print_header() -> None:
     print('')
     print('\t███████ ██████  ██ ██████  ███████ ██████')
     print('\t██      ██   ██ ██ ██   ██ ██      ██   ██')
@@ -37,20 +42,13 @@ def print_header():
     print('\t     ██ ██      ██ ██   ██ ██      ██   ██')
     print('\t███████ ██      ██ ██████  ███████ ██   ██')
     print('')
-                                           
-
-def print_crawling_options(options: dict):
-    print(f"Crawling {options['url']}", end="")
-    if options['recursive']:
-        print(f" recusively to depth {options['depth']}", end="")
-    print(f". Saving images to {options['path']}")
 
 # ---------------------------
 # Main
 # ---------------------------
-def main():
+def main() -> None:
     print_header()
-    args = parse_args()
+    args : Args = parse_args()
     print(args)
 
 if __name__ == '__main__':
